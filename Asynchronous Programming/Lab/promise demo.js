2393610803389424787e793121cc3038b55d9896
Petar Kobakov <pkobakov@gmail.com>
2023-10-31 08:51:09 +0200

    Added New Demo

---
+++ Asynchronous Programming/Lab/promise demo.js
@@ -0,0 +1,25 @@
+function executor(resolve, reject){
+    console.log('promise starting');
+    // setTimeout(() => {
+    //     resolve('Hello from the promise'), 5000
+    // });
+
+    setTimeout(() => {
+        reject(new Error('Something went wrong!')), 5000
+    });
+
+    console.log('promise ended');
+}
+
+const promise = new Promise(executor); 
+
+//promise.then(successCallback);
+promise.catch(failedCallBack);
+
+function successCallback(data){
+    console.log("RECEIVED DATA: ", data);
+}
+
+function failedCallBack(error){
+    console.error('Encountered error: ', error.message);
+}

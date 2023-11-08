a3410f4c944126817f78cd6f1a6c82dbf3c18b93
Petar Kobakov <pkobakov@gmail.com>
2023-11-08 12:59:15 +0200

    Added New Demo

---
+++ Single Page Applications/Lab/Demos/second demo/app.js
@@ -0,0 +1,43 @@
+// Manipulation via CSS
+
+// document.getElementById('home-link').addEventListener('click', () => {
+//     document.querySelectorAll('section').forEach(s => s.style.display = 'none');
+//     document.getElementById('home').style.display = 'block';
+// })
+
+// document.getElementById('catalog-link').addEventListener('click', () => {
+//     document.querySelectorAll('section').forEach(s => s.style.display = 'none');
+//     document.getElementById('catalog').style.display = 'block';
+// })
+
+// document.getElementById('login-link').addEventListener('click', () => {
+//     document.querySelectorAll('section').forEach(s => s.style.display = 'none');
+//     document.getElementById('login').style.display = 'block';
+// })
+//---------------------------------------------------------------------------------
+// Manipulate the DOM
+const main = document.getElementById('main');
+const views = {
+    home : document.getElementById('home'),
+    catalog: document.getElementById('catalog'),
+    login: document.getElementById('login')
+};
+
+Object.values(views).forEach(v => v.remove());
+
+main.replaceChildren(views.home);
+
+document.getElementById('home-link').addEventListener('click', ()=>{
+    main.replaceChildren(views.home)
+});
+
+document.getElementById('catalog-link').addEventListener('click', ()=>{
+    main.replaceChildren(views.catalog)
+});
+
+document.getElementById('login-link').addEventListener('click', ()=>{
+    main.replaceChildren(views.login)
+});
+
+
+

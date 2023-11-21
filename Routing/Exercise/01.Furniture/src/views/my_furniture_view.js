50d86d7fc32662066bf9786031c0f29a6371d3c7
Petar Kobakov <pkobakov@gmail.com>
2023-11-22 01:11:59 +0200

    Added My Publications

--- Routing/Exercise/01.Furniture/src/views/my_furniture_view.js
+++ Routing/Exercise/01.Furniture/src/views/my_furniture_view.js
@@ -1,3 +1,46 @@
-export function loadMyFurniture(){
-    console.log('my-furniture');
+import { html,render } from "../../node_modules/lit-html/lit-html.js";
+import { get } from "../api.js";
+import { userData } from "../user-data-helper.js";
+
+
+const root = document.querySelector('.container');
+
+function furnitureTemplate(data){
+    return html`
+    <div class="row space-top">
+        <div class="col-md-12">
+            <h1>My Furniture</h1>
+            <p>This is a list of your publications.</p>
+        </div>
+    </div>
+    <div class="row space-top">
+         ${data.map(item => cardTemplate(item))}
+    </div>
+    `;
+}
+
+function cardTemplate(item){
+    return html`
+    <div class="col-md-4">
+                <div class="card text-white bg-primary">
+                    <div class="card-body">
+                            <img src=${item.img} />
+                            <p>${item.description}</p>
+                            <footer>
+                                <p>Price: <span>${item.price} $</span></p>
+                            </footer>
+                            <div>
+                                <a href="/details/${item._id}" class="btn btn-info">Details</a>
+                            </div>
+                    </div>
+                </div>
+            </div>
+    `
+}
+
+export async function loadMyFurniture(){
+    const userId = userData.getUserId();
+    const data = await get(`data/catalog?where=_ownerId%3D%22${userId}%22`);
+    
+    render(furnitureTemplate(data), root);
 } 
